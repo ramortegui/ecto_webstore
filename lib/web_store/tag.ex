628b21de2c98520b312e896d056daae5aa38ed17
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-05-17 21:29:50 -0600

    Add tag schema

---
+++ lib/web_store/tag.ex
@@ -0,0 +1,13 @@
+defmodule WebStore.Tag do
+  use Ecto.Schema
+
+  schema "tags" do
+    field(:name, :string)
+  end
+
+  def changeset(tag, params \\ %{}) do
+    tag
+    |> Ecto.Changeset.cast(params, [:name])
+    |> Ecto.Changeset.validate_required([:name])
+  end
+end
